e2ace1e50f06a0f8321971d884a3d45a73edd0ac
sujjitha <sujithakonduru.ds@gmail.com>
2021-10-12 15:26:24 +0530

    Added logs to training.py file

--- src/training.py
+++ src/training.py
@@ -1,10 +1,17 @@
 import os
 from src.utils.data_mgmt import get_data
 from src.utils.model import create_model, save_model
-
+import logging
 from src.utils.common import read_config
 import argparse
 def training(config_path):
+    logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+    log_dir = "logs"
+    os.makedirs(log_dir, exist_ok=True)
+    logging.basicConfig(filename= os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str, filemode="a")
+
+
+
     config=read_config(config_path)
     validation_datasize = config["params"]["validation_datasize"]
     (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
@@ -12,13 +19,13 @@ def training(config_path):
     OPTIMIZER=config["params"]["optimizer"]
     METRICS=[config["params"]["metrics"]]
     NUM_CLASSES=config["params"]["num_classes"]
-
+    logging.info(f"loss function, optimizer, metrics and num classes from config.yaml file{LOSS_FUNCTION,OPTIMIZER,METRICS,NUM_CLASSES}")
     model=create_model(LOSS_FUNCTION,OPTIMIZER,METRICS,NUM_CLASSES)
     print("model_clf layer1 name  ",model.layers[1].name)
     weights, biases= model.layers[1].get_weights()
     print("weights shape: ",weights.shape)
     print("biases shape: ", biases.shape)
-
+    logging.info(f"weights and biases: {weights.shape, biases.shape}")
     EPOCHS=config["params"]["epochs"]
     VALIDATION_SET=(X_valid, y_valid)
     history=model.fit(X_train, y_train,epochs=EPOCHS, validation_data=VALIDATION_SET)
